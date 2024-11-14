346f2506fd451d073ce3b877a381d563acc42eea
Max F <116274825+macapac@users.noreply.github.com>
2024-11-14 14:17:32 +0100

    changes

--- basket.php
+++ basket.php
@@ -3,22 +3,22 @@ include("auth_session.php");
 include 'header.php'; // Include the header
 ?>
 
-  <main>
-  <!-- Row titles -->
-        <ul>
-          <li style="margin-left: 150px;" class="item item-heading">Item</li>
-          <li style="margin-left: 250px;" class="price">Price</li>
-        </ul>
-      </div>
+<main>
+	<!-- Row titles -->
+	<ul>
+		<li style="margin-left: 150px;" class="item item-heading">Item</li>
+		<li style="margin-left: 250px;" class="price">Price</li>
+	</ul>
+	</div>
 
-	<?php 
+	<?php
 	//hides errors
 	error_reporting(0);
 	//removing items from the basket
-    if(isset($_GET['remove']) && (!empty($_GET['remove'] || $_GET['remove'] ==0))){
+	if (isset($_GET['remove']) && (!empty($_GET['remove'] || $_GET['remove'] == 0))) {
 		unset($_SESSION['cart'][$_GET['remove']]);
 	}
-	if(empty($_SESSION['cart'])) {
+	if (empty($_SESSION['cart'])) {
 		//if cart is empty display the prices as 0
 		$total = 0;
 		$amount = 0;
@@ -28,7 +28,7 @@ include 'header.php'; // Include the header
 				  <p>&nbsp;</p>
 				  <p>&nbsp;</p>
                   </div>";
-	}else{
+	} else {
 		//if not empty, intialising variables
 		$total = 0;
 		$amount = 0;
@@ -39,41 +39,54 @@ include 'header.php'; // Include the header
 			require('db.php');
 			$query = "SELECT * FROM products WHERE ItemName = '$item'";
 			$result = mysqli_query($con, $query);
-			while($row = mysqli_fetch_array($result)) {
+			while ($row = mysqli_fetch_array($result)) {
 				$price = $row["Price"];
 				$total = $total + ($price * $no_items);
-		?>
-		<!-- Displaying items in the basket -->
-			<div class="w3-row-padding">
-				<div class="w3-display-container">
-					<div class ="styling">	
-						<div class="basket-product" ><div class="item"><div class="product-image" >	
-						<!-- Displaying item image -->						
-						<div style="display: inline-block" class="pic"><?php echo "<img src='img/".$row['Image']."' id='myimage'  height='210' width ='220'>"; ?> </div>
-						</div></div>
-						<strong><div class="product-details item-quantity">
-						<!-- Displaying item name -->	
-						<div class="w3-display-bottom w3-white w3-padding"><p><?php echo $row["ItemName"]; ?> </p> </div>
-						<!-- Displaying item size -->	
-						<div class="w3-display-bottom w3-white w3-padding"><p>Size <?php echo $row["Size"]; ?>  </p></div>
-						<!-- Displaying quantity -->
-						<?php echo "<strong><div class='qua product-details'> <p>Quantity :</p>$amount</div></strong>";?>	
-					<p>&nbsp;</p>
-					<p>&nbsp;</p>
-						<!-- Displaying remove button -->
-						<?php echo '<div><a class="remov" href="?remove=' . $item . '">Remove</a></div>'; ?>
+				?>
+				<!-- Displaying items in the basket -->
+				<div class="w3-row-padding">
+					<div class="w3-display-container">
+						<div class="styling">
+							<div class="basket-product">
+								<div class="item">
+									<div class="product-image">
+										<!-- Displaying item image -->
+										<div style="display: inline-block" class="pic">
+											<?php echo "<img src='img/" . $row['Image'] . "' id='myimage'  height='210' width ='220'>"; ?>
+										</div>
+									</div>
+								</div>
+								<strong>
+									<div class="product-details item-quantity">
+										<!-- Displaying item name -->
+										<div class="w3-display-bottom w3-white w3-padding">
+											<p><?php echo $row["ItemName"]; ?> </p>
+										</div>
+										<!-- Displaying item size -->
+										<div class="w3-display-bottom w3-white w3-padding">
+											<p>Size <?php echo $row["Size"]; ?> </p>
+										</div>
+										<!-- Displaying quantity -->
+										<?php echo "<strong><div class='qua product-details'> <p>Quantity :</p>$amount</div></strong>"; ?>
+										<p>&nbsp;</p>
+										<p>&nbsp;</p>
+										<!-- Displaying remove button -->
+										<?php echo '<div><a class="remov" href="?remove=' . $item . '">Remove</a></div>'; ?>
+									</div>
+									<div style="margin-left: 125px;" class="price">
+										<p>£ <?php echo $row["Price"] * $amount; ?> </p>
+									</div>
+								</strong>
+							</div>
 						</div>
-						<div style="margin-left: 125px;" class="price"><p>£ <?php echo $row["Price"]* $amount; ?> </p></div></strong>
 					</div>
-				</div>
-			</div>
-			
-			<?php
+
+					<?php
 			}
 		}
-			
+
 	}
-	
+
 	//making sure the decimal places for each price variable is the same
 	$total = number_format($total, 2);
 	$tax = number_format($tax, 2);
@@ -81,211 +94,39 @@ include 'header.php'; // Include the header
 	$ftotal = number_format($ftotal, 2);
 	//storing data into orders and orderline if the user clicks the submit button 
 	$cust_id = $_SESSION['cust_id'];
-	 if (isset($_POST['submitbutton'])){
-        require('db.php');
-        $query = "INSERT INTO orders (CustomerID, total) VALUES ($cust_id, $ftotal)";
+	if (isset($_POST['submitbutton'])) {
+		require('db.php');
+		$query = "INSERT INTO orders (CustomerID, total) VALUES ($cust_id, $ftotal)";
 		$result = mysqli_query($con, $query);
 		$order_id = mysqli_insert_id($con);
-		foreach($_SESSION['cart'] as $x => $val){
-            $item_id = $val['ID'];
-            $quantity = $val['Quantity'];
-            $query = "INSERT INTO order_line (OrderID, ItemID, Quantity) VALUES ($order_id, $item_id, $quantity)";
+		foreach ($_SESSION['cart'] as $x => $val) {
+			$item_id = $val['ID'];
+			$quantity = $val['Quantity'];
+			$query = "INSERT INTO order_line (OrderID, ItemID, Quantity) VALUES ($order_id, $item_id, $quantity)";
 			$result = mysqli_query($con, $query);
 		}
 		if ($result) {
 			//re direct user to the checkout page if the purchase is succesful 
-			echo'<script> window.location="checkout.php"; </script> ';
+			echo '<script> window.location="checkout.php"; </script> ';
 			//empty basket
 			empty($_SESSION['cart']);
 		}
-	 }
+	}
 	?>
-	<!-- Price box-->
-	<div class="summary">
-        <div class="summary-total-items"><span class="total-items"></span> Items in your Bag (<?php echo $no_items ?>) </div>
-        <div class="summary-subtotal">
-          <div class="subtotal-title">Subtotal</div>
-          <div class="subtotal-value final-value" id="basket-subtotal">£ <?php echo $total ; ?></div>
-        </div>		
-		<form method="post" name="purchase">
-        <input type="submit" style="cursor: pointer;" class ="checkout-cta summary-checkout" name="submitbutton" value="Checkout" />
-        </form>
+		<!-- Price box-->
+		<div class="summary">
+			<div class="summary-total-items"><span class="total-items"></span> Items in your Bag
+				(<?php echo $no_items ?>) </div>
+			<div class="summary-subtotal">
+				<div class="subtotal-title">Subtotal</div>
+				<div class="subtotal-value final-value" id="basket-subtotal">£ <?php echo $total; ?></div>
+			</div>
+			<form method="post" name="purchase">
+				<input type="submit" style="cursor: pointer;" class="checkout-cta summary-checkout" name="submitbutton"
+					value="Checkout" />
+			</form>
 
-	</main>
-</p>
-  </main>
+</main>
 </body>
 
 </html>
-<style>
-h2 {
-	font-size: 25px;
-	color:black;
-	font-weight:200;
-	margin-left: -90px;
-    padding-top: 30px;
-}
-.messa{
-	margin-left: 435px;
-	margin-top:150px;
-    width: 1000px;
-    padding: 0px 25px;
-	position:absolute:
-	font-size: 20px;	
-}
-.qua{
-	margin-left:0px;
-	margin-top:0px;
-}
-.product-details {
-  padding: 0em;
-  box-sizing: border-box;
-}
-.header-logo {
-    padding-top: 3px;
-	padding-bottom: 8px;
-}
-.nav li {
-  display: inline-block;
-  font-size: 15px;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  background-color: #f4f4f4;
-  letter-spacing: 0.5px;
-}
-.price{
-	text-align: center:
-}
-.imag {
-  display: block;
-  margin-left: auto;
-  padding-top: 0px;
-  padding-left: 60px;
-  padding-right: 0px;
-}
-.w10-padding-100 {
-	padding-top: 0px;
-	padding-bottom: 0px;
-} 
-.header-logoee {
-	color: white;
-	display: inline-block;
-	font-size: 10px;
-	padding-bottom: 0px;
-	padding-left: 20px;
-	padding-top: 0px;
-	background-color: black;
-	float: left;
-	text-decoration: none;
-	font-family: arial;
-}
-.header-logoe {
-	color: white;
-	display: inline-block;
-	font-size: 25px;
-	padding-left: 20px;
-	padding-top: 70px;
-	background-color: black;
-	text-decoration: none;
-	font-family: arial;
-}
-.dropdown {
-  position: relative;
-  display: inline-block;
-  margin: 20px;
-  padding: 5px 10px;
-}
-li a {
-  display: block;
-  color: black;
-  text-align: center;
-  padding: 8px 25px;
-  text-decoration: none;
-  background-color: rgba(244, 244, 244, 1);
-  font-family: arial;
-  font-weight: 0;
-  font-size: 12px;
-}
-li {
-  float: right;
-}
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  border: 1px #f4f4f4;
-  background-color: #f4f4f4;
-}
-@charset "utf-8";
-@import url(https://fonts.googleapis.com/css?family=Open+Sans:400,700,600);
-html,
-html a {
-  -webkit-font-smoothing: antialiased;
-  text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.004);
-}
-body {
-  background-color: #fff;
-  color: #666;
-  font-family: 'Open Sans', sans-serif;
-  font-size: 62.5%;
-  margin: 0 auto;
-  line-height: 1.5;
-}
-strong {
-  font-weight: bold;
-}
-p {
-  margin: 0.75rem 0 0;
-}
-h1 {
-  font-size: 0.75rem;
-  font-weight: normal;
-  margin: 0;
-  padding: 0;
-}
-main {
-  clear: both;
-  font-size: 0.85rem;
-  margin: 0 auto;
-  overflow: hidden;
-  padding: 1rem 0;
-  width: 100%;
-}
-ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-li {
-  color: #111;
-  display: inline-block;
-  padding: 0.5rem 0;
-}
-.item {
-  width: 55%;
-  margin-right: -350px;
-}
-.remove button {
-  background-color: transparent;
-  color: #777;
-  float: none;
-  text-decoration: underline;
-  text-transform: uppercase;
-}
-.summary-checkout {
-  display: block;
-}
-.checkout-cta {
-  display: block;
-  float: none;
-  font-size: 0.75rem;
-  text-align: center;
-  text-transform: uppercase;
-  padding: 0.625rem 0;
-  width: 100%;
-  background-color: #606060;
-  color: white;
-  padding:15px;
-}
-</style>
--- item.php
+++ item.php
@@ -4,371 +4,110 @@ include 'header.php'; // Include the header
 ?>
 
 <body>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <html>
 
-<?php
-// passing the itemID to this page as a parameter
-$var = $_GET['id'];
-?>
-<!-- Displaying the description of the item -->
-  <div class="w3-row-padding w3-padding-16 w3-center" id="food">
-    <div class="style-quarter">
-	<h1><?php echo $var; ?></h1>
-	<?php 
-	error_reporting(0);
-		require('db.php');
-		$query = "SELECT * FROM products WHERE ItemName = '$var'";
-		$result = mysqli_query($con, $query);
-		while($row = mysqli_fetch_array($result)) {
-				$stock = $row["Stock"];
-		?>
-	<div class="w3-row-padding">
-	<div class="w3-display-container">
-	<div class ="styling">			
-	<div class="pic"><?php echo "<img src='img/".$row['Image']."' id='myimage' height='490' width ='490'>"; ?> </div>  
-	<div style="text-transform:uppercase; font-size: 20px;" class="w3-display-bottom w3-white w3-padding"><?php echo $row["Brand"]; ?>  </div>					
-	<div style="font-weight: 700;" class="w3-display-bottom w3-white w3-padding"><p>£<?php echo $row["Price"]; ?> </p></div>
-	<p>Tax Included.</p>
-	<ol>
-	<div class="w3-display-bottom w3-white w3-padding"><p>• <?php echo $row["ItemName"]; ?> </p> </div>
-	<div class="w3-display-bottom w3-white w3-padding"><p>• Colour: <?php echo $row["Colour"]; ?> </p> </div>
-	<div class="w3-display-bottom w3-white w3-padding"><p>• Size: <?php echo $row["Size"]; ?>  </p></div>
-	<div class="w3-display-bottom w3-white w3-padding"><p>• Stock: <?php echo $stock; ?> </p> </div>
-	<div class="w3-display-bottom w3-white w3-padding"><p>• Condition: <?php echo $row["Condition"]; ?>  </p></div>
-	</ol>
-    </div>
-	</div>
-	</div>
-<?php
-	}
-?>
-    </div>
-	
-	<?php 
-	// quantity function
-	require('db.php');
-	// checks if the selected quantity that the user has chose is valid by checking the items stock in the database
-	$query = "SELECT * FROM products WHERE ItemName = '$var'";
-	$result = mysqli_query($con, $query);
-	while ($item = mysqli_fetch_array($result)){
-		if(isset($_POST['quantity']) && $_REQUEST['quantity'] > 0 && $_REQUEST['quantity'] <= $stock) {
-			$quantity=$_REQUEST['quantity'] ;
-			if(isset($_SESSION['cart'][$var])) {
-				$_SESSION['cart'][$var]['Quantity'] += $quantity;
-			}else{
-				if ($_SESSION['cart'][$var]['Quantity'] + $quantity > $stock){
-					echo "<div style='font-size:12px; font-family:sans-serif; text-align:center; padding: 40px; position:fixed; background-color:white; border-style: solid; border-color:black; margin-left:460px; margin-top:-300px;' class='mes'>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php
+    // passing the itemID to this page as a parameter
+    $var = $_GET['id'];
+    ?>
+    <!-- Displaying the description of the item -->
+    <div class="w3-row-padding w3-padding-16 w3-center" id="food">
+      <div class="style-quarter">
+        <h1><?php echo $var; ?></h1>
+        <?php
+        error_reporting(0);
+        require('db.php');
+        $query = "SELECT * FROM products WHERE ItemName = '$var'";
+        $result = mysqli_query($con, $query);
+        while ($row = mysqli_fetch_array($result)) {
+          $stock = $row["Stock"];
+          ?>
+          <div class="w3-row-padding">
+            <div class="w3-display-container">
+              <div class="styling">
+                <div class="pic"><?php echo "<img src='img/" . $row['Image'] . "' id='myimage' height='490' width ='490'>"; ?>
+                </div>
+                <div style="text-transform:uppercase; font-size: 20px;" class="w3-display-bottom w3-white w3-padding">
+                  <?php echo $row["Brand"]; ?> </div>
+                <div style="font-weight: 700;" class="w3-display-bottom w3-white w3-padding">
+                  <p>£<?php echo $row["Price"]; ?> </p>
+                </div>
+                <p>Tax Included.</p>
+                <ol>
+                  <div class="w3-display-bottom w3-white w3-padding">
+                    <p>• <?php echo $row["ItemName"]; ?> </p>
+                  </div>
+                  <div class="w3-display-bottom w3-white w3-padding">
+                    <p>• Colour: <?php echo $row["Colour"]; ?> </p>
+                  </div>
+                  <div class="w3-display-bottom w3-white w3-padding">
+                    <p>• Size: <?php echo $row["Size"]; ?> </p>
+                  </div>
+                  <div class="w3-display-bottom w3-white w3-padding">
+                    <p>• Stock: <?php echo $stock; ?> </p>
+                  </div>
+                  <div class="w3-display-bottom w3-white w3-padding">
+                    <p>• Condition: <?php echo $row["Condition"]; ?> </p>
+                  </div>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <?php
+        }
+        ?>
+      </div>
+
+      <?php
+      // quantity function
+      require('db.php');
+      // checks if the selected quantity that the user has chose is valid by checking the items stock in the database
+      $query = "SELECT * FROM products WHERE ItemName = '$var'";
+      $result = mysqli_query($con, $query);
+      while ($item = mysqli_fetch_array($result)) {
+        if (isset($_POST['quantity']) && $_REQUEST['quantity'] > 0 && $_REQUEST['quantity'] <= $stock) {
+          $quantity = $_REQUEST['quantity'];
+          if (isset($_SESSION['cart'][$var])) {
+            $_SESSION['cart'][$var]['Quantity'] += $quantity;
+          } else {
+            if ($_SESSION['cart'][$var]['Quantity'] + $quantity > $stock) {
+              echo "<div style='font-size:12px; font-family:sans-serif; text-align:center; padding: 40px; position:fixed; background-color:white; border-style: solid; border-color:black; margin-left:460px; margin-top:-300px;' class='mes'>
 						  <h3>Invalid Quantity</h3>
 						  <p>Click <a style='text-decoration:underline; 'href=''>Here</a> to return</p>
 						  </div>";
-				}else{
-					$_SESSION['cart'][$var] = array('ID' => $item["ItemID"], 'Quantity' => $quantity, 'Price' => $item["Price"]);
-				}
-			}
-		}else{
-		}
-	}
-?>
+            } else {
+              $_SESSION['cart'][$var] = array('ID' => $item["ItemID"], 'Quantity' => $quantity, 'Price' => $item["Price"]);
+            }
+          }
+        } else {
+        }
+      }
+      ?>
 
-<!-- Add to basket button, and quantity box-->
-<form action="" style="cursor: pointer; font-color:#606060;" method="post" name="addtobasket">
-	 <input  style="display:block; margin-left: 560px; margin-bottom:35px;" type="integer" name="quantity" value="1" size="2"/>	
-	 <a style="text-decoration: none; font-weight: 700; letter-spacing:.08em; color: #606060; display: inline-block;"><input class="summary-checkout checkout-cta" type="submit" value="add to cart"/></a>
-</form>  
+      <!-- Add to basket button, and quantity box-->
+      <form action="" style="cursor: pointer; font-color:#606060;" method="post" name="addtobasket">
+        <input style="display:block; margin-left: 560px; margin-bottom:35px;" type="integer" name="quantity" value="1"
+          size="2" />
+        <a
+          style="text-decoration: none; font-weight: 700; letter-spacing:.08em; color: #606060; display: inline-block;"><input
+            class="summary-checkout checkout-cta" type="submit" value="add to cart" /></a>
+      </form>
 
-<!-- Buy it now button-->
-<div style="cursor: pointer;"class="summary-checkout">
-<button type="submit" value="Checkout" class="checkout-ctar"><a style="text-decoration: none; font-weight: 700;letter-spacing:.08em; color: white;" href="checkout.php">Buy It Now</a></button>
-</div>	
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<!-- Back to shop button -->
-<div style="cursor: pointer;">
-<button class="backtoshop"><a style="text-decoration: none; font-weight: 700;letter-spacing:.08em; color: #3a3a3a;" href="shop.php" ><i class="arrow left"></i> Back To Shop</button></a>
-</div>
-
-
-<style>
-.style-quarter{
-  padding-left: 20px;
-  padding-right: 20px;
-  padding-top: 5px;
-  padding-bottom: 20px;
-  margin-left: 500px;
-  margin-right: 0px;
-}
-.mes{
-	margin-left:100px
-}
-.img-magnifier-glass {
-  position: absolute;
-  border-radius: 50%;
-  cursor: none;
-  width: 100px;
-  height: 100px;
-}
-.left {
-  transform: rotate(135deg);
-  -webkit-transform: rotate(135deg);
-}
-.arrow {
-  border: solid black;
-  border-width: 0 2px 2px 0;
-  display: inline-block;
-  padding: 3px;
-}
-body{
-	line-height:1.5;
-}
-.pic{
-	left: 0px;
-    width: 100px;
-    height: 120px;
-	position: absolute;
-	margin-top: -50px;
-}
-.styling {
-    font-family: sans-serif;
-	font-size: 17px;
-	font-weight: 400;
-	line-height: 1.5;
-	margin-left:30px;
-	color:black;
-}
-ol {
-  padding-right: 5px;
-  padding-left: 5px;
-  background-color: white;
-  margin-left: 0px;
-  font-family: sans-serif;
-  font-weight: 400;
-  font-size: 15px;
-  line-height: 1.5;
-}
-.backtoshop{
-  display: block;
-  float: none;
-  font-size: 13px;
-  font-family:Amiri,serif;
-  text-align: center;
-  text-transform: uppercase;
-  padding: 15px 10px;
-  width: 20%;
-  background-color:#fff;
-  border-color#3a3a3a;
-  border-radius: 2px; 
-  border-width: 1px;
-  cursor: pointer;
-  margin-left:450px;	
-}
-.summary-checkout {
-  display: block;
-  width:42%;
-  padding-left: 5px;
-  padding-right: 5px;
-  line-height:1.4;
-  margin-left: 550px;
-  margin-right: 250px;
-  margin-bottom:10px;
-  cursor: pointer;
-}
-.checkout-cta {
-  display: block;
-  float: none;
-  font-size: 15px;
-  color:#606060;
-  font-family:Amiri,serif;
-  text-align: center;
-  text-transform: uppercase;
-  padding: 15px 10px;
-  width: 100%;
-  background-color:#fff;
-  border-color:#606060;
-  border-radius: 2px; 
-  border-width: 1px;
-  cursor: pointer;
-  font-weight: 700;
-  width:48%;
-  padding-left: 10px;
-}
-.checkout-ctar {
-  display: block;
-  float: none;
-  font-size: 15px;
-  font-family:Amiri,serif;
-  text-align: center;
-  text-transform: uppercase;
-  padding: 15px 10px;
-  width: 100%;
-  background-color: #606060;
-  border-color#3a3a3a;
-  border-radius: 2px;  
-  border: 1px solid transparent;
-  cursor: pointer;
-  margin-left:-5px;
-}
-body{
-	margin: 0px;
-}
-#logo{
-	padding-top: 3px;
-	padding-bottom: 8px;
-	cursor:pointer;
-	float:left;
-	width:25%;
-}
-.dropbtn {
-  background-color: #4CAF50;
-  color: white;
-  padding: 5px;
-  font-size: 16px;
-  border: none;
-  cursor: pointer;
-}
-.dropdown {
-  position: relative;
-  display: inline-block;
-  margin: 20px;
-  padding: 5px 10px;
-}
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-.dropdown-content a {
-  color: black;
-  padding: 5px 16px;
-  text-decoration: none;
-  display: block;
-}
-.dropdown-content a:hover {background-color: #f1f1f1;}
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-.dropdown:hover .dropbtn {
-  background-color: #f1f1f1;
-}
-.w10-padding-100 {
-	padding-top: 0px;
-	padding-bottom: 0px;
-}
-.header-logoe {
-	color: white;
-	display: inline-block;
-	font-size: 25px;
-	padding-left: 20px;
-	padding-top: 70px;
-	background-color: black;
-	text-decoration: none;
-	font-family: arial;
-}
-.header-logoee {
-	color: white;
-	display: inline-block;
-	font-size: 10px;
-	padding-bottom: 0px;
-	padding-left: 20px;
-	padding-top: 0px;
-	background-color: black;
-	float: left;
-	text-decoration: none;
-	font-family: arial;
-}
-.w3-padding-50 {
-	padding-top: 50px;
-	padding-bottom: 50px;
-}	
-.w3-padding-100 {
-	padding-top: 0px;
-	padding-bottom: 250px;
-}
-.w3-padding-15{
-	padding-top: 30px;
-	padding-bottom: 16px;
-}
-.w4-padding {
-	padding-top:0px ;
-	padding-bottom:0px;
-	padding-left: 15px;
-}	
-.w10-container {
-	text-align: center;
-	padding-bottom: 26px;
-	padding-left: 100px;
-	padding-right: 100px;
-}
-.w12-padding-16 {
-	padding: 48px;
-	font-size:40px;
-	font-weight: 400;
-	margin-bottom: 0px;
-	font-family: sans-serif;
-	line-height: 1;
-}
-.nav li {
-  display: inline-block;
-  font-size: 15px;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  background-color: #f4f4f4;
-  letter-spacing: 0.5px;
-}
-ul {
-  list-style-type: none;
-  text-align: center;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  border: 1px #f4f4f4;
-  background-color: #f4f4f4;
-}
-li {
-  float: right;
-}
-
-li a {
-  display: block;
-  color: black;
-  text-align: center;
-  padding: 8px 25px;
-  text-decoration: none;
-  background-color: rgba(244, 244, 244, 1);
-  font-family: arial;
-  font-weight: 0;
-  font-size: 12px;
-}
-h1 {
-	font-family: sans-serif;
-	font-size:28px;
-	color: black;
-	font-weight: 300;
-	margin-left:30px;
-}		
-.header-logo {
-    padding-top: 3px;
-	padding-bottom: 8px;
-}
-.word {
-	color: white;
-	display: inline-block;
-	font-size: 20px;
-	text-decoration: none;
-	text-align: center;
-	letter-spacing: 2px;
-	cursor: pointer;
-}
-.imag {
-  display: block;
-  margin-left: auto;
-  padding-top: 0px;
-  padding-left: 60px;
-  padding-right: 0px;
-}
-
-</style>
+      <!-- Buy it now button-->
+      <div style="cursor: pointer;" class="summary-checkout">
+        <button type="submit" value="Checkout" class="checkout-ctar"><a
+            style="text-decoration: none; font-weight: 700;letter-spacing:.08em; color: white;" href="checkout.php">Buy
+            It Now</a></button>
+      </div>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <!-- Back to shop button -->
+      <div style="cursor: pointer;">
+        <button class="backtoshop"><a
+            style="text-decoration: none; font-weight: 700;letter-spacing:.08em; color: #3a3a3a;" href="shop.php"><i
+              class="arrow left"></i> Back To Shop</button></a>
+      </div>
 </body>
