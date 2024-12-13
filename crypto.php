9b76506536f4b2fecd1ec15eac439797a21995f2
Max F <116274825+macapac@users.noreply.github.com>
2024-12-13 01:28:07 +0100

    working

--- crypto.php
+++ crypto.php
@@ -3,19 +3,17 @@ function generateKeyPair() {
     $config = [
         "private_key_bits" => 2048,
         "private_key_type" => OPENSSL_KEYTYPE_RSA,
+        "config" => 'C:/xampp/php/extras/openssl\openssl.cnf', // Update this path
     ];
 
-    // Generate private key
     $privateKeyResource = openssl_pkey_new($config);
 
     if (!$privateKeyResource) {
         throw new Exception("Failed to generate private key: " . openssl_error_string());
     }
 
-    // Extract private key as a string
-    openssl_pkey_export($privateKeyResource, $privateKey);
+    openssl_pkey_export($privateKeyResource, $privateKey, NULL, $config);
 
-    // Extract public key from the private key
     $publicKeyDetails = openssl_pkey_get_details($privateKeyResource);
     $publicKey = $publicKeyDetails['key'];
 
@@ -24,4 +22,5 @@ function generateKeyPair() {
         'publicKey' => $publicKey
     ];
 }
+
 ?>
